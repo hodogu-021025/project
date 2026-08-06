79090293863fe6cc91225dd84d1d2326953d9eee
H021025 <tonytony1424@gmail.com>
2026-08-06 16:00:33 +0900

    Add files via upload

---
+++ JavaProject/src/com/test/java/member/Member.java
@@ -0,0 +1,75 @@
+package com.test.java.member;
+
+public class Member {
+
+	private String name1;
+	private String name2;
+	private int score1;
+	private int score2;
+	private int setscore1;
+	private int setscore2;
+	
+	public Member(String name1, String name2, int score1, int score2, int setscore1, int setscore2) {
+		this.name1 = name1;
+		this.name2 = name2;
+		this.score1 = score1;
+		this.score2 = score2;
+		this.setscore1 = setscore1;
+		this.setscore2 = setscore2;
+	}
+
+	public String getName1() {
+		return name1;
+	}
+
+	public void setName1(String name1) {
+		this.name1 = name1;
+	}
+
+	public String getName2() {
+		return name2;
+	}
+
+	public void setName2(String name2) {
+		this.name2 = name2;
+	}
+
+	public int getScore1() {
+		return score1;
+	}
+
+	public void setScore1(int score1) {
+		this.score1 = score1;
+	}
+
+	public int getScore2() {
+		return score2;
+	}
+
+	public void setScore2(int score2) {
+		this.score2 = score2;
+	}
+
+	public int getSetscore1() {
+		return setscore1;
+	}
+	
+	public void setSetscore1(int setscore1) {
+		this.setscore1 = setscore1;
+	}
+	
+	public int getSetscore2() {
+		return setscore2;
+	}
+	
+	public void setSetscore2(int setscore2) {
+		this.setscore2 = setscore2;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [name1=" + name1 + ", name2=" + name2 + ", score1=" + score1 + ", score2=" + score2
+				+ ", setscore1=" + setscore1 + ", setscore2=" + setscore2 + "]";
+	}
+
+}
---
+++ JavaProject/src/com/test/java/service/gameService.java
@@ -0,0 +1,250 @@
+package com.test.java.service;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.test.java.data.Data;
+import com.test.java.member.Member;
+import com.test.java.ui.UI;
+
+public class gameService {
+
+	public static void gameRunningSingle() {
+	    Data data = new Data();
+	    List<Member> list = data.list();
+	    Member member = list.get(0);
+	    
+	    Scanner scan = new Scanner(System.in);
+	    boolean loop = true;
+	    
+	    serveService.ServeStart();
+	    
+	    while (loop) {
+	        int s1 = member.getScore1();
+	        int s2 = member.getScore2();
+
+	        boolean isNormalWin1 = (s1 == 11 && s2 < 10);
+	        boolean isNormalWin2 = (s2 == 11 && s1 < 10);
+
+	        boolean isDeuceWin1 = (s1 >= 10 && s2 >= 10) && (s1 - s2 == 2);
+	        boolean isDeuceWin2 = (s1 >= 10 && s2 >= 10) && (s2 - s1 == 2);
+
+	        int round = member.getSetscore1() + member.getSetscore2() + 1;
+	        
+	        // 1번 선수 세트 승리
+	        if (isNormalWin1 || isDeuceWin1) {
+	        	member.setSetscore1(member.getSetscore1() + 1);
+	            member.setScore1(0);
+	            member.setScore2(0);
+	            data.saveFile(member);
+	            
+	            // 최종 우승 
+	            if (member.getSetscore1() == 1) { 
+	                UI.sendFinalWinner(member.getName1());
+	                loop = false;
+	            } else {
+	                menuService.scoreList();
+	            }
+	            continue;
+	        }
+
+	        // 2번 선수 세트 승리
+	        if (isNormalWin2 || isDeuceWin2) {
+	            member.setSetscore2(member.getSetscore2() + 1);
+	            member.setScore1(0);
+	            member.setScore2(0);
+	            data.saveFile(member);
+	            
+	            // 최종 우승 
+	            if (member.getSetscore2() == 1) { 
+	                UI.sendFinalWinner(member.getName2());
+	                loop = false;
+	            } else {
+	            	menuService.scoreList();
+	            }
+	            continue; 
+	        }
+
+	        //듀스
+	        if (s1 >= 10 && s2 >= 10 && s1 == s2) {
+	            UI.deuceUI();
+	        }
+	        
+	        System.out.print("🔥 득점한 선수: ");
+	        String player = scan.next();
+
+	        if (player.equals(member.getName1())) {
+	            member.setScore1(member.getScore1() + 1);
+	            UI.sendMessage(member.getName1() + " 선수 득점!!\r\n");
+	            data.saveFile(member);
+	            menuService.scoreList();
+	        } else if (player.equals(member.getName2())) {
+	            member.setScore2(member.getScore2() + 1);
+	            UI.sendMessage(member.getName2() + " 선수 득점!!\r\n");
+	            data.saveFile(member);
+	            menuService.scoreList();
+	        } else {
+	            UI.sendWarning("선수의 이름을 정확히 입력하세요\r\n");
+	        }
+	        
+	        int totalScore = member.getScore1() + member.getScore2();
+	        // 서브 듀스 상황
+	        if (member.getScore1() >= 10 && member.getScore2() >= 10) {
+	        	if (totalScore % 2 == 0) {
+	        		serveService.serveNum = 1;
+	        		UI.sendMessage(member.getName1() + " 선수 서브입니다!\r\n");
+	        	} else {
+	        		serveService.serveNum = 2;
+	        		UI.sendMessage(member.getName2() + " 선수 서브입니다!\r\n");
+	        	}
+	        } 
+	        // 서브 일반 상황
+	        else {
+	        	if (totalScore % 4 < 2) {
+	        		serveService.serveNum = 1;
+	        		UI.sendMessage(member.getName1() + " 선수 서브입니다!\r\n");
+	        	} else {
+	        		serveService.serveNum = 2;
+	        		UI.sendMessage(member.getName2() + " 선수 서브입니다!\r\n");
+	        	}
+	        }
+	    } // while
+	}
+	
+	public static void gameRunningThree() {
+	    Data data = new Data();
+	    List<Member> list = data.list();
+	    Member member = list.get(0);
+	    
+	    Scanner scan = new Scanner(System.in);
+	    boolean loop = true;
+	    
+	    serveService.ServeStart();
+	    
+	    while (loop) {
+	        int s1 = member.getScore1();
+	        int s2 = member.getScore2();
+
+	        boolean isNormalWin1 = (s1 == 11 && s2 < 10);
+	        boolean isNormalWin2 = (s2 == 11 && s1 < 10);
+
+	        boolean isDeuceWin1 = (s1 >= 10 && s2 >= 10) && (s1 - s2 == 2);
+	        boolean isDeuceWin2 = (s1 >= 10 && s2 >= 10) && (s2 - s1 == 2);
+
+	        int round = member.getSetscore1() + member.getSetscore2() + 1;
+	        
+	        // 1번 선수 세트 승리
+	        if (isNormalWin1 || isDeuceWin1) {
+	        	member.setSetscore1(member.getSetscore1() + 1);
+	            member.setScore1(0);
+	            member.setScore2(0);
+	            data.saveFile(member);
+	            
+	            // 최종 우승 
+	            if (member.getSetscore1() == 2) { 
+	                UI.sendFinalWinner(member.getName1());
+	                loop = false;
+	            } else {
+	                menuService.scoreList();
+	            }
+	            continue;
+	        }
+
+	        // 2번 선수 세트 승리
+	        if (isNormalWin2 || isDeuceWin2) {
+	            member.setSetscore2(member.getSetscore2() + 1);
+	            member.setScore1(0);
+	            member.setScore2(0);
+	            data.saveFile(member);
+	            
+	            // 최종 우승 
+	            if (member.getSetscore2() == 2) { 
+	                UI.sendFinalWinner(member.getName2());
+	                loop = false;
+	            } else {
+	            	menuService.scoreList();
+	            }
+	            continue; 
+	        }
+
+	        //듀스
+	        if (s1 >= 10 && s2 >= 10 && s1 == s2) {
+	            UI.deuceUI();
+	        }
+	        
+	        System.out.print("🔥 득점한 선수: ");
+	        String player = scan.next();
+
+	        if (player.equals(member.getName1())) {
+	            member.setScore1(member.getScore1() + 1);
+	            UI.sendMessage(member.getName1() + " 선수 득점!!\r\n");
+	            data.saveFile(member);
+	            menuService.scoreList();
+	        } else if (player.equals(member.getName2())) {
+	            member.setScore2(member.getScore2() + 1);
+	            UI.sendMessage(member.getName2() + " 선수 득점!!\r\n");
+	            data.saveFile(member);
+	            menuService.scoreList();
+	        } else {
+	            UI.sendWarning("선수의 이름을 정확히 입력하세요\r\n");
+	        }
+	        
+	        int totalScore = member.getScore1() + member.getScore2();
+	        // 서브 듀스 상황
+	        if (member.getScore1() >= 10 && member.getScore2() >= 10) {
+	        	if (totalScore % 2 == 0) {
+	        		serveService.serveNum = 1;
+	        		UI.sendMessage(member.getName1() + " 선수 서브입니다!\r\n");
+	        	} else {
+	        		serveService.serveNum = 2;
+	        		UI.sendMessage(member.getName2() + " 선수 서브입니다!\r\n");
+	        	}
+	        } 
+	        // 서브 일반 상황
+	        else {
+	        	if (totalScore % 4 < 2) {
+	        		serveService.serveNum = 1;
+	        		UI.sendMessage(member.getName1() + " 선수 서브입니다!\r\n");
+	        	} else {
+	        		serveService.serveNum = 2;
+	        		UI.sendMessage(member.getName2() + " 선수 서브입니다!\r\n");
+	        	}
+	        }
+	    } // while
+	}
+	
+	public static void gameStart() {
+		
+		Data data = new Data();
+	    List<Member> list = data.list();
+		
+	    Member member = list.get(0);
+	    
+	    // 세트 미설정 예외 처리
+	    if (menuService.roundType.equals("")) {
+	    	
+	    	UI.sendWarning("세트를 설정하세요.");
+	    	
+    	// 단판 승부
+	    } else if (menuService.roundType.equals("단판 승부")) {
+	    	
+	    	if (member.getName1().equals("등록된 선수가 없습니다!")||member.getName2().equals("등록된 선수가 없습니다!")) {
+		        UI.sendWarning("등록된 선수가 없습니다!");
+		    } else {
+		    	UI.startUI(menuService.roundType);
+		    	gameRunningSingle();
+		    }
+    	
+    	// 3판 2선승
+	    } else if (menuService.roundType.equals("3판 2선승")) {
+		    if (member.getName1().equals("등록된 선수가 없습니다!")||member.getName2().equals("등록된 선수가 없습니다!")) {
+		        UI.sendWarning("등록된 선수가 없습니다!");
+		    } else {
+		    	UI.startUI(menuService.roundType);
+		    	gameRunningThree();
+		    }
+	}
+}
+	
+	
+}
